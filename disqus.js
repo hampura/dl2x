cce53e6f2ff7baa3f82bafbacb0a42002410ca71
hampura <32120029+hampura@users.noreply.github.com>
2025-04-09 06:30:08 +0700

    Add files via upload

--- disqus.js
+++ disqus.js
@@ -4,57 +4,47 @@ function hapus0() {
 	}
 }
 
-console.info(
-	'%cKomentar Disqus telah terhubung dengan ' + dl2xFtl,
-	'color:cyan'
-);
+function komentarLama() {
+	switch (dl2xFtl) {
+		case 'apkomik': dl2xUser = 'komikav-com', pageUrl = 'https://' + dl2xDomain + '/' + dl2xJudul.toLowerCase().replace(/\s+/g, '-') + '-chapter-' + chFile + '-bahasa-indonesia'; break
+		case 'kiryuu': hapus0(); dl2xUser = 'kiryuu-id', pageUrl = 'https://' + dl2xDomain + '/' + dl2xJudul.toLowerCase().replace(/\s+/g, '-') + '-chapter-' + chFile; break
+		case 'komikcast': dl2xUser = 'komikcastnet', pageUrl = 'https://' + dl2xDomain + '/chapter/' + dl2xJudul.toLowerCase().replace(/\s+/g, '-') + '-chapter-' + chFile + '-bahasa-indonesia'; break
+		case 'komikindo':
+		case 'komiksin': dl2xUser = 'komikindo-co', pageUrl = 'https://' + dl2xDomain + '/' + dl2xJudul.toLowerCase().replace(/\s+/g, '-') + '-chapter-' + chFile; break
+		case 'mangakita': dl2xUser = 'mangakita', pageUrl = 'https://' + dl2xDomain + '/' + dl2xJudul.toLowerCase().replace(/\s+/g, '-') + '-chapter-' + chFile; break
+		case 'mangatale': dl2xUser = 'baca-manga-4', pageUrl = 'https://' + dl2xDomain + '/' + dl2xJudul.toLowerCase().replace(/\s+/g, '-') + '-chapter-' + chFile; break
+		case 'shinigami': dl2xUser = 'reaperid', 	pageUrl = 'https://home.shinigami.cx/series/' + dl2xJudul.toLowerCase().replace(/\s+/g, '-') + '/chapter-' + chFile; break
+		case 'tukangkomik': dl2xUser = 'tukang', pageUrl = 'https://' + dl2xDomain + '/' + dl2xJudul.toLowerCase().replace(/\s+/g, '-') + '-chapter-' + chFile;
+	}
+}
+
+function komentarBaru() {
+	switch (dl2xFtl) {
+		case 'apkomik':		dl2xUser = 'komikav-com'; break
+		case 'kiryuu':		dl2xUser = 'kiryuu-id'; break
+		case 'komikcast':	dl2xUser = 'komikcastnet', pageUrl = 'https://' + dl2xDomain + '/chapter/' + dl2xJudul; break
+		case 'komikindo':
+		case 'komiksin':	dl2xUser = 'komikindo-co'; break
+		case 'mangakita':	dl2xUser = 'mangakita'; break
+		case 'mangatale':	dl2xUser = 'baca-manga-4'; break
+		case 'shinigami':	dl2xUser = 'dewakematians', dl2xJudul = $('del').text(), pageUrl = 'https://dsq.shinigami.gg/chapter/' + dl2xJudul; break
+		case 'tukangkomik':	dl2xUser = 'tukang';
+	}
+}
 
 $('button[onclick="maps"]').attr('onclick', 'maps()'), $('#info').click(function() {
 	$('#komentarDisqus').slideDown()
 }), $('#tutupKomentar').click(function() {
 	$('#komentarDisqus').slideUp()
-});
+}); pageUrl = 'https://' + dl2xDomain + '/' + dl2xJudul; console.info(
+	'%cKomentar telah terhubung ke:\n' + pageUrl,
+	'color:cyan'
+);
 
-switch (dl2xFtl) {
-	case 'apkomik':
-		dl2xUser = 'komikav-com',
-		pageUrl = 'https://' + dl2xDomain + '/' + dl2xJudul.toLowerCase().replace(/\s+/g, '-') + '-chapter-' + chFile + '-bahasa-indonesia';
-		break;
-	case 'kiryuu':
-		hapus0();
-		dl2xUser = 'kiryuu-id',
-		pageUrl = 'https://' + dl2xDomain + '/' + dl2xJudul.toLowerCase().replace(/\s+/g, '-') + '-chapter-' + chFile;
-		break;
-	case 'komikcast':
-		dl2xUser = 'komikcastnet',
-		pageUrl = 'https://' + dl2xDomain + '/chapter/' + dl2xJudul.toLowerCase().replace(/\s+/g, '-') + '-chapter-' + chFile + '-bahasa-indonesia';
-		break;
-	case 'komikindo':
-	case 'komiksin':
-		dl2xUser = 'komikindo-co',
-		pageUrl = 'https://' + dl2xDomain + '/' + dl2xJudul.toLowerCase().replace(/\s+/g, '-') + '-chapter-' + chFile;
-		break;
-	case 'mangakita':
-		dl2xUser = 'mangakita',
-		pageUrl = 'https://' + dl2xDomain + '/' + dl2xJudul.toLowerCase().replace(/\s+/g, '-') + '-chapter-' + chFile;
-		break;
-	case 'mangatale':
-		dl2xUser = 'baca-manga-4',
-		pageUrl = 'https://' + dl2xDomain + '/' + dl2xJudul.toLowerCase().replace(/\s+/g, '-') + '-chapter-' + chFile;
-		break;
-	case 'shinigami':
-		if ($('del').length === 1) {
-			dl2xJudul = $('del').text(),
-			dl2xUser = 'dewakematians',
-			pageUrl = 'https://dsq.shinigami.gg/chapter/' + dl2xJudul;
-		} else {
-			dl2xUser = 'reaperid',
-			pageUrl = 'https://home.shinigami.cx/series/' + dl2xJudul.toLowerCase().replace(/\s+/g, '-') + '/chapter-' + chFile;
-		}
-		break;
-	case 'tukangkomik':
-		dl2xUser = 'tukang',
-		pageUrl = 'https://' + dl2xDomain + '/' + dl2xJudul.toLowerCase().replace(/\s+/g, '-') + '-chapter-' + chFile;
+if ($('del').length === 1) {
+	komentarBaru()
+} else {
+	komentarLama()
 }
 
 var disqus_config = function() {
